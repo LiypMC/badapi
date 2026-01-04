afe274a8ad84fe747747bde13378c50e80816a6d
Naol <139908666+LiypMC@users.noreply.github.com>
2026-01-03 21:01:03 -0500

    fixed some errors

--- badapi-front/.next/server/webpack-runtime.js
+++ badapi-front/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3e82ee63c8c666c7")
+/******/ 		__webpack_require__.h = () => ("f9696620c58533a8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- badapi-front/.next/static/chunks/webpack.js
+++ badapi-front/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e48a4afb014b48e"; }
+/******/ 		__webpack_require__.h = function() { return "a4e10afb7963bcb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
